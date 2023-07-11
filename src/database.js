f7d242d2644434e70f261802d55b9052cd34aed8
khanhbui <khanhbd@bizflycloud.vn>
2023-07-11 18:10:43 +0700

    update

--- src/database.js
+++ src/database.js
@@ -1,6 +1,28 @@
 const { MongoClient } = require('mongodb')
 let cachedDb
 
+async function setupDb() {
+  try {
+    const [callConfigs, phoneReports] = await Promise.allSettled([
+      cachedDb.collection('configs').findOne({ type: 'call-config' }),
+      cachedDb.collection('phone-reports').findOne()
+    ])
+
+    if (!callConfigs.value) {
+      await cachedDb.collection('configs').insertOne({
+        type: 'call-config',
+        duration: { min: 10, max: 100 },
+        delay: { min: 10, max: 100 }
+      })
+    }
+    if (!phoneReports.value) {
+      await cachedDb.collection('phone-reports').insertOne({})
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 async function connectDb() {
   if (cachedDb) {
     return cachedDb
@@ -10,6 +32,8 @@ async function connectDb() {
   await client.connect()
 
   cachedDb = client.db(process.env.DB_NAME)
+
+  setupDb()
 
   return cachedDb
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,7 @@
 exports.randomInRange = (min, max) => {
   return (Math.random() * (max - min)) + min
 }
+
+exports.testCommand = () => {
+  console.log('command run')
+}
